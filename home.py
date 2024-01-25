71904dc72c261646fe43085a384e1ec39c068142
Tofunori <124537575+tofunori@users.noreply.github.com>
2024-01-25 13:54:53 -0500

    Update home.py

--- home.py
+++ home.py
@@ -3,3 +3,5 @@ import leafmap.foliumap as leafmap
 import geopandas as gpd
 
 st.title('PICOM - Cartographie des données')
+
+st.write("Le lien Github pour le code Python du projet: https://github.com/tofunori/Multi-buffers-data-extraction ")
