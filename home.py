e779c933a77ab4c843f20e92e0e4ec3fa4de5a9a
Tofunori <124537575+tofunori@users.noreply.github.com>
2024-01-25 20:53:44 -0500

    zip

--- home.py
+++ home.py
@@ -4,11 +4,12 @@ import geopandas as gpd
 
 st.title('PICOM - Cartographie des données')
 
-st.write("Le lien Github pour le code Python du projet: https://github.com/tofunori/Multi-buffers-data-extraction ")
+# Path to the existing ZIP file
+zip_file_path = 'files/picom_data.zip'  # Replace with your ZIP file path
 
-zip_file_path = "files/picom_data.zip"
+
 # Create two columns
-col1, col2 = st.columns([1, 1], gap="small")  # Adjust the ratio as needed
+col1, col2 = st.columns([3, 1])  # Adjust the ratio as needed
 
 # Put the text in the first column
 col1.write("Télécharger les données Shapefile et excel:")
@@ -17,8 +18,8 @@ col1.write("Télécharger les données Shapefile et excel:")
 with open(zip_file_path, "rb") as file:
     # Create the download button in the second column
     col2.download_button(
-        label="data.zip",
+        label="Données Shapefile",
         data=file,
-        file_name="picom_data.zip",
+        file_name="data.zip",
         mime="application/zip"
     )
