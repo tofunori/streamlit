8f1a664ba9c4c4a5e16d0d35485c05e48ba8d7ca
Tofunori <124537575+tofunori@users.noreply.github.com>
2024-01-25 21:19:46 -0500

    Update charts.py

--- pages/charts.py
+++ pages/charts.py
@@ -1,9 +1,21 @@
 import streamlit as st
-import leafmap.foliumap as leafmap
-import geopandas as gpd
 import altair as alt
+import geopandas as gpd
 
-gpd.read_csv("files/csv/landcover_1km.csv")
+# Load the data
+landcover = gpd.read_file("files/roads_buffers_1km.shp")
 
+# Convert to a Pandas DataFrame and assume 'landcover_type' is an attribute to plot
+landcover_df = landcover.drop(columns='geometry')
+landcover_counts = landcover_df.groupby('Site')['length'].sum().reset_index()
+landcover_counts.columns = ['Site', 'length']
 
-alt.Chart(gpd.read_csv("files/csv/landcover_1km.csv")).mark_bar()
+# Create an Altair bar chart
+chart = alt.Chart(landcover_counts).mark_bar().encode(
+    x='Site',
+    y='length'
+)
+
+# Display the chart in Streamlit
+st.altair_chart(chart, use_container_width=True)
+
